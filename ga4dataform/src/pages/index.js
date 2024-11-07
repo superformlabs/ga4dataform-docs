91334f39623608c7cf662c4c5cadc8c82063788c
simonbreton <simon@privatemailto.me>
2024-11-07 11:14:36 -0600

    small fixed

--- ga4dataform/src/pages/index.js
+++ ga4dataform/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="ga4dataform/">
+            to="docs">
             GA4 Dataform Doc
           </Link>
         </div>
